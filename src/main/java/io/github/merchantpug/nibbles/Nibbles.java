e8d444ee9b26583eb866a2001d5521a5d5f17061
Pug <81167232+MerchantPug@users.noreply.github.com>
2021-07-14 04:08:58 +1000

    Removed unnecessary imports

--- src/main/java/io/github/merchantpug/nibbles/Nibbles.java
+++ src/main/java/io/github/merchantpug/nibbles/Nibbles.java
@@ -1,8 +1,6 @@
 package io.github.merchantpug.nibbles;
 
 import net.fabricmc.api.ModInitializer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class Nibbles implements ModInitializer {
 
