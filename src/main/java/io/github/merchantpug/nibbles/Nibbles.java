a6b089361069057bc5c97e8cb94abc27ac456e19
Pug <81167232+MerchantPug@users.noreply.github.com>
2021-07-14 04:08:04 +1000

    Removed initialize message

--- src/main/java/io/github/merchantpug/nibbles/Nibbles.java
+++ src/main/java/io/github/merchantpug/nibbles/Nibbles.java
@@ -5,10 +5,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Nibbles implements ModInitializer {
-	public static Logger LOGGER = LogManager.getLogger(Nibbles.class);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Nibbles has been initialized. Eat your heart out!");
+
 	}
 }
